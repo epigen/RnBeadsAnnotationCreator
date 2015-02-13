9aae824ddd268056c851b2749a838c20f9cc03f0
fmueller <fmueller@5ea55d32-e619-49da-8562-93c7a683c930>
2015-02-13 13:10:27 +0000

    RnBeads annotation creator package scaffold

---
+++ R/RnBeads/RnBeadsAnnotationCreator/R/createAnnotationPackage.hg38.R
@@ -0,0 +1,10 @@
+#' createAnnotationPackage.hg38
+#' 
+#' Helper function to create annotation package for genome assembly hg38
+#' RnBeads annotation for that assembly
+#' @param dest destination directory where the package should be generated
+#' @return invisible \code{TRUE} if successful
+#' @author Yassen Assenov, Fabian Mueller
+createAnnotationPackage.hg38 <- function(dest=getwd()){
+	invisible(TRUE)
+}
