ed36b110b409e8cea9c4a9cb0709424623bd575b
yassen <yassen@5ea55d32-e619-49da-8562-93c7a683c930>
2015-02-13 13:15:34 +0000

    Global settings

---
+++ R/globals.R
@@ -0,0 +1,73 @@
+########################################################################################################################
+## globals.R
+## created: 2012-08-18
+## creator: Yassen Assenov
+## ---------------------------------------------------------------------------------------------------------------------
+## Global variables used by functions related to the update of probe annotations.
+########################################################################################################################
+
+########################################################################################################################
+## Environment containing global variables for the targeted genome assembly
+
+.globals <- new.env()
+
+########################################################################################################################
+## Predefined constants used in RnBeads
+
+## Dinucleotide patterns to be annotated
+NUCLEOTIDE.PATTERNS <- c("CG")
+names(NUCLEOTIDE.PATTERNS) <- sapply(strsplit(NUCLEOTIDE.PATTERNS, ""), paste, collapse = "p")
+
+## Length, in base pairs, of a neighborhood of targeted CpG dinucleotide; used in calculating GC content and CpG density
+LENGTH.NEIGHBORHOOD <- 100L
+
+## Length, in base pairs, of tiling regions
+LENGTH.TILING <- 5000L
+
+## Length, in base pairs, of a gene promoter region upstream of the TSS
+LENGTH.PROMOTER.UPSTREAM <- 1500L
+
+## Length, in base pairs, of a gene promoter region downstream of the TSS
+LENGTH.PROMOTER.DOWNSTREAM <- 500L
+
+## Maximum distance, in base pairs, from a CpG island that defines a CGI shore
+LENGTH.CGI.SHORE <- 2000L
+
+## Maximum distance, in base pairs, from a CpG island shore that defines a CGI shelf
+LENGTH.CGI.SHELF <- 2000L
+
+########################################################################################################################
+## dbSNP
+
+## Base FTP location of dbSNP
+DBSNP.FTP.BASE <- "ftp://ftp.ncbi.nih.gov/snp/organisms/"
+
+## FTP location of BED files for human in dbSNP
+DBSNP.FTP.VCF <- list(
+	"hg19" = paste0(DBSNP.FTP.BASE, "human_9606/VCF/", "00-All.vcf.gz"),
+	"mm10" = paste0(DBSNP.FTP.BASE, "mouse_10090/VCF/", "vcf_chr_", c(1:19, "X", "Y"), ".vcf.gz"),
+	"rn5" = paste0(DBSNP.FTP.BASE, "rat_10116/VCF/", "vcf_chr_", c(1:20, "X"), ".vcf.gz"))
+rm(DBSNP.FTP.BASE)
+
+########################################################################################################################
+## Ensembl
+
+.globals[['ENSEMBL.DATABASE']] <- "ensembl"
+
+## Additional variables that could/must be defined in the assembly-specific header:
+##   ENSEMBL.HOST         # Host URL (precise version of Ensembl) to be used
+## * ENSEMBL.DATASET      # Name of the gene dataset within the "ensembl" database
+## * ENSEMBL.GENE.ATTRS   # Attributes (columns) to download from Ensembl for gene definitions
+
+########################################################################################################################
+## UCSC Genome Browser
+
+## Base FTP location of files from the UCSC Genome Browser
+UCSC.FTP.BASE <- "ftp://hgdownload.cse.ucsc.edu/goldenPath/"
+
+## Text file containing the CpG Island track in the UCSC Genome Browser
+UCSC.FTP.CGIS <- paste0(UCSC.FTP.BASE, assembly, "/database/cpgIslandExt.txt.gz")
+
+## Text file containing the definition of chromosome bands
+UCSC.FTP.BANDS <- paste0(UCSC.FTP.BASE, assembly, "/database/cytoBand.txt.gz")
+rm(UCSC.FTP.BASE)
