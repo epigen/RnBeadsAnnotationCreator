b461c78824fabf6ecaf6944b1f537cd4ef9a1220
fmueller <fmueller@5ea55d32-e619-49da-8562-93c7a683c930>
2015-03-09 16:17:10 +0000

    adapted hg38 creator according to the hg19 template fixed a bug in region annotation (tiling) that originated in incompatible chromosome names

--- R/createAnnotationPackage.hg38.R
+++ R/createAnnotationPackage.hg38.R
@@ -23,9 +23,15 @@ createAnnotationPackage.hg38 <- function(dest){
 	## Supported chromosomes
 	assignChromosomes(c(1:22, "X", "Y"))
 
-	## TODO: Download genes from Ensembl
+	## Download SNP annotation
+	logger.start("SNP Annotation")
+	vcf.files <- paste0(DBSNP.FTP.BASE, "human_9606_b141_GRCh38/VCF/", "All.vcf.gz")
+	snps <- update.annot("snps", "polymorphism information", rnb.update.dbsnp, ftp.files = vcf.files)
+	logger.completed()
+
+	## Download genes from Ensembl
 	biomart.parameters <- list(
-		database.name = "ensembl",
+		database.name = "ENSEMBL_MART_ENSEMBL",
 		dataset.name = "hsapiens_gene_ensembl",
 		required.columns = c(
 			"id" = "ensembl_gene_id",
@@ -34,11 +40,15 @@ createAnnotationPackage.hg38 <- function(dest){
 			"end" = "end_position",
 			"strand" = "strand",
 			"symbol" = "hgnc_symbol",
-			"entrezID" = "entrezgene"))
+			"entrezID" = "entrezgene"),
+		host = "dec2014.archive.ensembl.org")
 
 	logger.start("Region Annotation")
 	regions <- update.annot("regions", "region annotation", rnb.update.region.annotation,
 		biomart.parameters = biomart.parameters)
 	logger.completed()
 
+	logger.start("Genomic Sites")
+	sites <- update.annot("sites", "CpG annotation", rnb.update.sites, cpgislands = regions[["cpgislands"]])
+	logger.completed()
 }
