fb12ccf39bcd731ecce230835af88c382e9bdf46
yassen <yassen@5ea55d32-e619-49da-8562-93c7a683c930>
2015-04-16 14:37:43 +0000

    Fixed a logger bug when the temp/ directory is removed.

--- R/createAnnotationPackage.R
+++ R/createAnnotationPackage.R
@@ -86,25 +86,18 @@ rnb.export.annotations.to.data.files <- function() {
 #' @param assembly    Targeted genome assembly. Must be one of \code{"hg38"}, \code{"hg19"}, \code{"mm10"},
 #'                    \code{"mm9"}, \code{"rn5"}.
 #' @param dest        Destination directory where the package should be generated.
-#' @param cores.count Number of processing cores to be used in the computations.
 #' @return None (invisible \code{NULL}).
 #' @author Fabian Mueller
 #' @examples
 #' createAnnotationPackage("hg38")
 #' @export
-createAnnotationPackage <- function(assembly,dest=getwd(),cores.count=1L){
+createAnnotationPackage <- function(assembly,dest=getwd()){
 	## Validate parameters
 	if (!(is.character(assembly) && length(assembly) == 1 && (isTRUE(assembly != "")))) {
 		stop("invalid value for assembly")
 	}
 	if (!(is.character(dest) && length(dest) == 1 && (isTRUE(dest != "")))) {
 		stop("invalid value for dest")
-	}
-	if (is.double(cores.count) && isTRUE(all(cores.count == as.integer(cores.count)))) {
-		cores.count <- as.integer(cores.count)
-	}
-	if (!(is.integer(cores.count) && length(cores.count) == 1 && (isTRUE(cores.count >= 1L)))) {
-		stop("invalid value for cores.count")
 	}
 
 	## Validate the genome assembly
@@ -113,39 +106,41 @@ createAnnotationPackage <- function(assembly,dest=getwd(),cores.count=1L){
 		stop("Unsupported assembly")
 	}
 
-	## Initialize the environment variables and the logger
-	assign("assembly", assembly, .globals)
+	## Initialize the environment variables
+	rm(list = ls(.globals), pos = .globals)
+	assign('assembly', assembly, .globals)
 	dir.package <- file.path(dest, paste0("RnBeads.", assembly))
-	assign("DIR.PACKAGE", dir.package, .globals)
-
-	## Initialize parallel processing
-	if (cores.count != 1) {
-		registerDoParallel(cores.count)
-	}
+	assign('DIR.PACKAGE', dir.package, .globals)
 
 	## Initialize package directories
 	if (file.exists(dir.package)) {
 		dir.package.state <- "(existing)"
 	} else {
 		if (!createPackageScaffold(paste0("RnBeads.", assembly), dest = dest)) {
-			rm(list = c("assembly", "DIR.PACKAGE"), pos = .globals)
 			stop("Could not create package directory")
 		}
 		dir.package.state <- "(created)"
 	}
 
 	## Create the annotation package
-	logger.start("Creating Annotation Package", fname = c(file.path(dir.package, "temp", "annotation.log"), NA))
+	logging2console <- (!logger.isinitialized())
+	if (logging2console) {
+		logger.start(fname = NA)
+	}
+	logger.start("Creating Annotation Package")
 	logger.info(c("Assembly:", assembly))
 	logger.info(c("Package directory", dir.package.state, ":", dir.package))
 	rm(dir.package, dir.package.state)
 	do.call(function.name, list())
 
 	## Clean the temporary directory
-	if (unlink(file.path(.globals[["DIR.PACKAGE"]], "temp"), recursive = TRUE) != 0L) {
+	if (unlink(file.path(.globals[['DIR.PACKAGE']], "temp"), recursive = TRUE) != 0L) {
 		logger.status("Cleaned package temporary directory")
 	} else {
 		logger.error("Could not clean package temporary directory")
 	}
 	logger.completed()
+	if (logging2console) {
+		logger.close()
+	}
 }
